66dd3fa8e0796952ddb2aa93a853b007a20493c7
EvgeniyGagen <evg961@mail.ru>
2022-12-06 22:51:42 +0300

    *

---
+++ Seminar1.py/DomaskaSeminar5.py/5_1.py
@@ -0,0 +1,13 @@
+#  напишите программу, удаляющую из текста все слова, содержащие "абв".
+
+
+
+
+print()
+print('1.Задача: Удалить из текста все слова, содержащие ""абв""')
+with open('text.txt', 'r', encoding = 'utf_8') as data:
+    stroka = data.read().split()
+print(f'В файле записано: {stroka}')
+print('Удалили все слова с абв и получили: ')
+print(' '.join([word for word in stroka if 'абв' not in word]))
+print()
